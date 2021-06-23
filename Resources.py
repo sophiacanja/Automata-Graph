63aec41d3005c4d80d4aaa88d82d77645e620e87
bathong188 <lebathong188@gmail.com>
2021-06-23 16:13:10 -0700

    update resources class

--- Resources.py
+++ Resources.py
@@ -3,17 +3,17 @@ import Node,Arc
 class Resources(Node, Arc): #resources is a subclass of both node and arc class
 
     def ___init__(self,resources):
-        self.resources = {}     # ex: {"R1":2,"R3":4}
+        self.__dict__ = resources    # ex: {"R1":2,"R3":4}
         
     def setResourceFreq(self):
         pass
         
-    def getResourceFreq(self):
-        pass
+    def getResourceFreq(self, resourceName):
+        return self.resources.get(resourceName)
     
     def getResourceList(self, resources):
         return self.resources
 
 
 myResource = Resources({'R1' : 2, 'R3' : 4})
- 
+print(myResource.getResourceFreq)

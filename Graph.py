153e4a584e405fc81570397ef2851df8409caafe
sophiacanja <sophia.canja1@gmail.com>
2021-06-24 22:25:45 -0700

    added nodeObjects list

--- Graph.py
+++ Graph.py
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
-class Graph:
-    def __init__(self,trace,resourceSet ):
-        
-        
-=======
 class Graph: 
+    nodeObjects = []
 
     def __init__(self, traces, resources):
         self.traces = traces
@@ -42,4 +37,3 @@ resources: [{"R1":15,"R2":8,"R3":12,"R6":4,"R8":6,"R9":11,"R12":24,"R14":5,"R15"
             {"R1":2,"R2":1,"R3":1,"R8":1,"R9":2,"R11":6,"R14":1,"R15":2,},
             {"R4":1,"R6":1,"R8":2,"R13":1,}]
 """
->>>>>>> Stashed changes

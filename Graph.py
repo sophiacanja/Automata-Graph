d2680b53c01d5d52413996d06e3134fe52e5f6b4
bathong188 <lebathong188@gmail.com>
2021-06-25 21:42:02 -0700

    added unique nodes

--- Graph.py
+++ Graph.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Graph: 
     nodeObjects = []
 
@@ -13,8 +15,11 @@ class Graph:
     def createArcs(): # helper method
         pass
 
-    def createNodes(): # helper method
-        pass
+    def createNodes(self): # helper method
+        uniqueNodes = np.unique(np.array([elem for singleList in self.traces for elem in singleList]))
+        print(uniqueNodes)
+        for node in uniqueNodes:
+            
 
     def isValidTrace():
         return False
@@ -22,7 +27,8 @@ class Graph:
     def getTraceFrequency(trace):
         pass
 
-# myVar = Graph([["B","C","G"],["A","C","G"],["B","C","E","G"],["A","C","E","H"],["B","D","F","G"],["A","D","F","G"]],[{"R1":15,"R2":8,"R3":12,"R6":4,"R8":6,"R9":11,"R12":24,"R14":5,"R15":2},{"R2":10,"R5":24,"R6":3,"R7":12,"R9":4,"R11":22,"R14":8,},{"R3":2,"R6":12,"R12":1,"R15":36,},{"R1":12,"R6":3,"R9":1,},{"R1":2,"R2":1,"R3":1,"R8":1,"R9":2,"R11":6,"R14":1,"R15":2,},{"R4":1,"R6":1,"R8":2,"R13":1,}])
+myVar = Graph([["B","C","G"],["A","C","G"],["B","C","E","G"],["A","C","E","H"],["B","D","F","G"],["A","D","F","G"]],[{"R1":15,"R2":8,"R3":12,"R6":4,"R8":6,"R9":11,"R12":24,"R14":5,"R15":2},{"R2":10,"R5":24,"R6":3,"R7":12,"R9":4,"R11":22,"R14":8,},{"R3":2,"R6":12,"R12":1,"R15":36,},{"R1":12,"R6":3,"R9":1,},{"R1":2,"R2":1,"R3":1,"R8":1,"R9":2,"R11":6,"R14":1,"R15":2,},{"R4":1,"R6":1,"R8":2,"R13":1,}])
+myVar.createNodes()
 
 
 """ 

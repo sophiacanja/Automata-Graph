1959816350e5b2c02c96f987a7cbac182278634e
sophiacanja <sophia.canja1@gmail.com>
2021-06-27 12:59:02 -0700

    put nodeObjects[] in constructor and created a double for loop in createArcs()
    Put node objects in the constructor so that we can access the nodeObjects in diff methods

--- Graph.py
+++ Graph.py
@@ -2,10 +2,11 @@ import Node
 import numpy as np
 
 class Graph: 
-    
+   
     def __init__(self, traces, resources):
         self.traces = traces
         self.resources = resources
+        self. nodeObjects = []
         # self.inputs = []
         # self.outputs = []
         # self.data = data
@@ -20,16 +21,26 @@ class Graph:
         pass
 
 
-    def createArcs(): # helper method
-        pass
+    def createArcs(self): # helper method
+        print(self.traces)
+        for currTrace in self.traces:               #forloop to traverse each trace in self.traces
+            for currActivity in currTrace:
+                print(currActivity)
+            print("new trace started")
+        
+        
+        
 
     def createNodes(self): # helper method
         uniqueNodes = np.unique(np.array([elem for singleList in self.traces for elem in singleList]))
-        nodeObjects = []
         for nodeName in uniqueNodes:
             newNode = Node.Node(nodeName)
-            nodeObjects.append(newNode)
-        return nodeObjects 
+            self.nodeObjects.append(newNode)
+       
+        # for i in self.nodeObjects:  #printing out node objects
+        #      print(i.activity)
+        # print("COMPLETED")
+   
 
     def isValidTrace():
         return False
@@ -40,6 +51,7 @@ class Graph:
 myVar = Graph([["B","C","G"],["A","C","G"],["B","C","E","G"],["A","C","E","H"],["B","D","F","G"],["A","D","F","G"]],[{"R1":15,"R2":8,"R3":12,"R6":4,"R8":6,"R9":11,"R12":24,"R14":5,"R15":2},{"R2":10,"R5":24,"R6":3,"R7":12,"R9":4,"R11":22,"R14":8,},{"R3":2,"R6":12,"R12":1,"R15":36,},{"R1":12,"R6":3,"R9":1,},{"R1":2,"R2":1,"R3":1,"R8":1,"R9":2,"R11":6,"R14":1,"R15":2,},{"R4":1,"R6":1,"R8":2,"R13":1,}])
 
 myVar.createNodes()
+myVar.createArcs()
 
 # myVar.buildGraph()
 
